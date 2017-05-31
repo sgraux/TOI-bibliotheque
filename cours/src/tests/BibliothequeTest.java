6bb96541563aba7319df93223cfaa956ee88eb82
Daniel <Daniel@DESKTOP-U6A9ESM>
2017-05-31 15:54:43 +0200

    JUnit 2

--- cours/src/tests/BibliothequeTest.java
+++ cours/src/tests/BibliothequeTest.java
@@ -2,13 +2,25 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import toi.Bibliotheque;
+import toi.Etagere;
 
 public class BibliothequeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void ajouterEtagere() {
+		Bibliotheque b = new Bibliotheque();
+		Etagere e= new Etagere(2);
+		ArrayList<Etagere> ar = new ArrayList<Etagere>();
+		
+		ar.add(e);
+		b.ajouterEtagere(e);
+		
+		assertEquals(b.get_listeEtagere(), ar);
 	}
 
 }
