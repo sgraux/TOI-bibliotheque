d0e3fab451ffa2111d44d8e150f529e103304cb4
quentinBdr <quentin.beudard.dev@gmail.com>
2017-05-31 15:58:34 +0200

    javadoc

--- cours/src/toi/Bibliotheque.java
+++ cours/src/toi/Bibliotheque.java
@@ -6,11 +6,23 @@ import java.util.Iterator;
 public class Bibliotheque {
 	
 	private ArrayList<Etagere> listeEtagere;
-	
+
+	/*
+	Constructeur, instancie une arraylist d'Etagere
+	 */
 	public Bibliotheque(){
 		listeEtagere = new ArrayList<Etagere>();
 	}
-	
+
+	/*
+	Recherche dans la arraylist un document
+	@param parDoc
+		Document a chercher
+	@return true
+		Document trouvé
+	@return false
+		Document absent
+	 */
 	public boolean rechercheDoc(Document parDoc){
 		Iterator iterateur = listeEtagere.iterator();
 		while(iterateur.hasNext()){
@@ -19,7 +31,14 @@ public class Bibliotheque {
 		}
 		return false;
 	}
-	
+
+	/*
+	Recherche dans la arraylist si un document porte le nom d'un auteur
+	@param parAuteur
+		Nom de l'auteur a chercher
+	@return true
+		Auteur d'un document trouvé
+	 */
 	public boolean rechercheAuteur(String parAuteur){
 		Iterator iterateur = listeEtagere.iterator();
 		while(iterateur.hasNext()){
@@ -28,7 +47,10 @@ public class Bibliotheque {
 		}
 		return false;
 	}
-	
+	/*
+	@param parAuteur
+		Nom de l'auteur a chercher
+	 */
 	public void ajouterEtagere(Etagere parEtagere){
 		listeEtagere.add(parEtagere);
 	}
